f8c7940a8d2aac72e17a7a9d0b9b0082a38b48f9
aris.goico@gmail.com <aris.goico@gmail.com@d4af04a9-09e7-65bd-d593-b4091ec9102f>
2014-01-22 15:58:28 +0000

    Primer commit de código con el prototipo hasta ahora.

---
+++ Proyecto Unity/Assets/Scripts/DeadZoneTrigger.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeadZoneTrigger : MonoBehaviour {
+
+	private bool ended = false;
+	private float timeRecord = -1f;
+
+
+	void OnTriggerEnter(Collider other) {
+		if (other.tag == "Player") {
+			GameObject.Destroy(other.gameObject);
+			Debug.Log("You Lose!");
+			Time.timeScale = 0f;
+			ended = true;
+			timeRecord = Time.time;
+		}
+	}
+
+	void OnGUI() {
+		if (ended) {
+			GUILayout.BeginArea(new Rect(Screen.width / 2 - 200f, Screen.height / 2 - 100f, 400f, 200f));
+			GUILayout.BeginVertical();
+			GUILayout.Label("You Lose!");
+			GUILayout.Label("Your time record is " + timeRecord + " seconds.");
+			if (GUILayout.Button("Restart?")) {
+				Time.timeScale = 1f;
+				Application.LoadLevel(Application.loadedLevel);
+			}
+			GUILayout.EndVertical();
+			GUILayout.EndArea();
+		}
+	}
+}
---
+++ Proyecto Unity/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public float speed = 1f;
+
+	// Use this for initialization
+	void Start () {
+		if (this.rigidbody == null) {
+			Debug.LogError("The player needs to have a Rigidbody attached. Disabling.");
+			this.enabled = false;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.rigidbody.AddForce(new Vector3(Input.GetAxis("Horizontal") * Time.deltaTime * speed, Input.GetAxis("Vertical") * Time.deltaTime * speed, 0f));
+	}
+}
